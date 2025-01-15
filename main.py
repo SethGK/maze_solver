b06445765cf843ec021e446247b796ffc316c27b
SethK99 <sgkloster@gmail.com>
2025-01-15 11:35:12 -0800

    added Cell class

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 import tkinter
 from window import Window
-from lines import Point, Line
+from lines import Point, Line, Cell
 
 def main():
     window = Window(800, 600)
     
-    l = Line(Point(50, 50), Point(400, 400))
-    window.draw_line(l, "black")
+
+    c = Cell(window)
+    c.has_left_wall = False
+    c.draw(50, 50, 100, 100)
+
+    c = Cell(window)
+    c.has_right_wall = False
+    c.draw(125, 125, 200, 200)
+
+    c = Cell(window)
+    c.has_bottom_wall = False
+    c.draw(225, 225, 250, 250)
+
+    c = Cell(window)
+    c.has_top_wall = False
+    c.draw(300, 300, 500, 500)
     
     window.wait_for_close()
 
--- window.py
+++ window.py
@@ -6,21 +6,20 @@ class Window:
         self.root = Tk()
         self.root.title("Maze Solver")
         self.canvas = Canvas(self.root, width=width, height=height)
-        self.canvas.pack()
+        self.canvas.pack(fill=BOTH,expand=1)
         self.root.running = True
         self.root.protocol("WM_DELETE_WINDOW", self.close)
 
     def redraw(self):
-        self.root.update()
         self.root.update_idletasks()
-
-    def draw_line(self, line, fill_color="black"):
-        line.draw(self.canvas, fill_color)
-        self.redraw()
+        self.root.update()
 
     def wait_for_close(self):
         while self.root.running:
             self.redraw()
+
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.canvas, fill_color)
 
     def close(self):
         self.root.running = False
